29564adf6cb71ee9e5062055183c21e47d228ab0
wmxl <294036748@qq.com>
2019-06-12 14:54:28 +0800

    [RY] finish countLifeNums and Test about it

--- src/main/java/ThoughtWorks04/App.java
+++ src/main/java/ThoughtWorks04/App.java
@@ -6,8 +6,61 @@ package ThoughtWorks04;
  */
 public class App 
 {
+
+    static int[][] mat = {
+            {1, 0, 1},
+            {1, 0, 1},
+            {1, 0, 1}
+    };
+
+    public static boolean judge(int x, int y){
+        if(x >= 0 && x <= 2 && y >= 0 && y <= 2)
+            return true;
+        return false;
+    }
+
+    /**
+     * 统计每个细胞周围活着的个数
+     * @param x 横坐标
+     * @param y 纵坐标
+     * @return
+     */
+    public static int countLifedNum(int x, int y){
+        int num=0;
+
+        if(judge(x-1, y-1))
+            num += mat[x-1][y-1];
+        if(judge(x, y-1))
+            num += mat[x][y-1];
+        if(judge(x+1, y-1))
+            num += mat[x+1][y-1];
+        if(judge(x-1, y))
+            num += mat[x-1][y];
+        if(judge(x+1, y))
+            num += mat[x+1][y];
+        if(judge(x-1, y+1))
+            num += mat[x-1][y+1];
+        if(judge(x, y+1))
+            num += mat[x][y+1];
+        if(judge(x+1, y+1))
+            num += mat[x+1][y+1];
+
+        return num;
+    }
+
+    public static void  transform(){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(countLifedNum(i, j)+ " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        transform();
+
     }
 }
--- src/test/java/ThoughtWorks04/AppTest.java
+++ src/test/java/ThoughtWorks04/AppTest.java
@@ -1,5 +1,7 @@
 package ThoughtWorks04;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -12,9 +14,34 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+    static int[][] mat = {
+            {1, 0, 1},
+            {1, 0, 1},
+            {1, 0, 1}
+    };
+    static int[][] lifeNum = {
+            {1, 4, 1},
+            {2, 6, 2},
+            {1, 4, 1}
+    };
+
     @Test
     public void shouldAnswerWithTrue()
-    {
+    {;
         assertTrue( true );
+    }
+    @Test
+    public void testCountLifedNum(){
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+                assertThat(lifeNum[i][j], is(App.countLifedNum(i, j)));
+
+
+            }
+        }
+
+
     }
 }
